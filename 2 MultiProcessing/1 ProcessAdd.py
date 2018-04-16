f0242b0f11b08950e196a325ec225a66543e2fb9
chenglin5580 <chenglin5580@163.com>
2018-04-16 22:11:33 +0800

    2018.04.16 multiprocessing Queue学习

--- 2 MultiProcessing/1 ProcessAdd.py
+++ 2 MultiProcessing/1 ProcessAdd.py
@@ -2,11 +2,22 @@
 
 import multiprocessing as mp
 
-def job(a, d):
+def job(a, d, q):
     print('aaaa')
+    res = a + d
+    q.put(res)
+
 
 
 if __name__=='__main__':
-    p1 = mp.Process(target=job, args=(1, 2))
+
+    q = mp.Queue()
+    p1 = mp.Process(target=job, args=(1, 2, q))
+    p2 = mp.Process(target=job, args=(3, 4, q))
     p1.start()
+    p2.start()
     p1.join()
+    p2.join()
+    res1 = q.get()
+    res2 = q.get()
+    print('res1', res1, 'res2', res2)
