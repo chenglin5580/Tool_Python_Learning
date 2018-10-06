3f827e58680eff553076a52124651ba0bb915d79
chenglin5580 <chenglin5580@163.com>
2018-10-06 15:19:36 +0800

    2018.10.06 加入Jupyter Notebook

--- 9 Scikit-Learn/1 DataPreProcessing/test.py
+++ 9 Scikit-Learn/1 DataPreProcessing/test.py
@@ -1,12 +1,9 @@
-
 from sklearn import preprocessing
 import numpy as np
 
-
 X_origin = np.array([[1., -1., 2.],
-                     [ 2.,  0.,  0.],
-                     [ 0.,  1., -1.]])
-
+                     [2., 0., 0.],
+                     [0., 1., -1.]])
 
 min_max_scaler = preprocessing.MinMaxScaler([0, 4])
 X_train_minmax = min_max_scaler.fit_transform(X_origin)
@@ -19,9 +16,10 @@ X_trans_origin = min_max_scaler.inverse_transform(X_transform)
 
 print(X_trans_origin)
 
-
 print(min_max_scaler.feature_range)
 
 print(min_max_scaler.data_min_)
 print(min_max_scaler.data_max_)
 print(min_max_scaler.scale_)
+
+
